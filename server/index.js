38cbe3a58342bf48be790b56f338001364fc277f
Cooled Monkey <lemonacidic@mail.ru>
2026-02-05 18:26:16 +0300

    feat: homework 1 add server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import * as dotenv from 'dotenv'
+import sequelize from './config/database.js';
+import cors from 'cors'
+import router from './routes/index.js'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+    try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+        const app = express();
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Ура, все заработало!'})
+})
+        app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+    } catch(e){
+        console.log(e);
+    }
+}
+
+start();
